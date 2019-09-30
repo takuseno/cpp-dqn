5fce0a12c21f160b2e6d6ed54a29d9d0be2dace6
takuseno <takuma.seno@gmail.com>
2019-09-30 15:08:39 +0900

    Enable xwindow at docker

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -eux
 
-sudo docker run -it --rm --runtime nvidia --name cpp-dqn takuseno/cpp-dqn:latest bash
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  _DISPLAY="$(ifconfig en0 | grep "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | sed -e "s/ netmask .*$//g" | sed -e "s/.*inet //g"):0"
+  socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+else
+  _DISPLAY=$DISPLAY
+fi
+
+sudo docker run \
+  -it \
+  --rm \
+  -v /tmp/.X11-unix/:/tmp/.X11-unix \
+  --shm-size=256m \
+  -e QT_X11_NO_MITSHM=1 \
+  -e DISPLAY=$_DISPLAY \
+  -v ${PWD}:/cpp-dqn \
+  --name cpp-dqn \
+  $* \
+  takuseno/cpp-dqn:latest bash
